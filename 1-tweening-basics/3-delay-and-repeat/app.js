d888e5728ee3a5f674919c49cfda413c0f94099c
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-11-18 08:16:49 +0000

    feat: Added delay and repeat

---
+++ 1-tweening-basics/3-delay-and-repeat/app.js
@@ -0,0 +1,19 @@
+// gsap.to(".fred", { x: 300, delay: 1, duration: 3 });
+// gsap.to(".fred", { x: 300, repeat: 1, duration: 3 });
+// gsap.to(".fred", { x: 300, repeat: 1, yoyo: true, duration: 3 });
+// gsap.to(".fred", { x: 300, delay: 1, repeat: 1, yoyo: true, duration: 3 });
+// gsap.to(".fred", {
+//   x: 300,
+//   repeat: -1,
+//   yoyo: true,
+//   repeatDelay: 1,
+//   duration: 3,
+// });
+gsap.to(".fred", {
+  x: 300,
+  delay: 1,
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 1,
+  duration: 3,
+});
